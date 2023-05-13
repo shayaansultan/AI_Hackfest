bf688beecf21e6d4ccf0267b8464e8efd7dc35c3
Yuv Bindal <yuv2bindal@gmail.com>
2023-05-13 16:33:44 +0530

    changes

--- testing.py
+++ testing.py
@@ -2,40 +2,20 @@ import easyocr
 import PIL
 from PIL import ImageDraw
 import numpy as np
-import re
 
-# Load image and extract text using EasyOCR
 reader = easyocr.Reader(['en'], gpu=False)
-im = PIL.Image.open("/content/veggie-grocery-receipt_orig.png")
+im = PIL.Image.open("/reciept1.png")
 im_np = np.array(im)
 bounds = reader.readtext(im_np)
 
-# Draw bounding boxes around text
 def draw_boxes(image, bounds, color='red', width=2):
-  draw = ImageDraw.Draw(image)
+    draw = ImageDraw.Draw(image)
 
-  for bound in bounds:
-    p0, p1, p2, p3 = bound[0]
-    draw.line([*p0, *p1, *p2, *p3, *p0], fill=color, width=width)
-  
-  return image
-
-im_with_boxes = draw_boxes(im, bounds)
-
-# Extract product names and prices
-reciept_text = ""
-for i in range(len(bounds)):
-  reciept_text += str(bounds[i][1]) + " "
-
-product_pattern = r'[A-Z ]+'
-price_pattern = r'\d+\.\d{2}'
-product_prices = re.findall(product_pattern + r'\n' + price_pattern, reciept_text)
-
-# Print extracted products and prices
-for item in product_prices:
-  product, price = item.split('\n')
-  print(f'Product: {product}, Price: {price}')
-
+    for bound in bounds:
+        p0, p1, p2, p3 = bound[0]
+        draw.line([*p0, *p1, *p2, *p3, *p0], fill=color, width=width)
+    
+    return image
 
 receipt_text = ""
 for i in range(len(bounds)):
